0e7b2601f93c7e8f183bc34540300896eecfcaac
Фирсов Михаил <firsov62121@gmail.com>
2022-07-22 10:53:22 +0300

    Change config files

--- server/src/db/create.js
+++ server/src/db/create.js
@@ -1,7 +1,13 @@
 const { sequelize } = require('./models');
+const { sequelizeInfo } = require('../config/index')
 
 async function main() {
-    await sequelize.sync({ force: true });
+    if(sequelizeInfo.syncType === 'FORCE')
+        await sequelize.sync({ force: true });
+    else if(sequelizeInfo.syncType === 'ALTER')
+        await sequelize.sync({ alter: true });
+    else
+        await sequelize.sync();
 }
 
 main()
