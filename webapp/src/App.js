629672ae26e11ff7774996025f60c10cc18fafc9
astatochek <71458498+astatochek@users.noreply.github.com>
2022-07-23 12:19:11 +0300

    svg and testing added

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { Nav } from './components';
+import { Nav, BoardComponent } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './routes/About/About'
 import Main from './routes/Main/Main'
+import Board from './routes/Board [testing]/Board';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,9 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Main></Main>}></Route>
 					<Route path="/about" element={<About></About>}></Route>
+					
+					{/* Placeholder */}
+					<Route path="/board" element={<Board />} /> 
 				</Routes>
 			</div>
 		</BrowserRouter>
---
+++ webapp/src/routes/Board [testing]/Board.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BoardComponent from "../../components/Board/BoardComponent";
+import "../../components/Board/style.css"
+
+const Board = () => {
+    return (
+        <div>
+            <BoardComponent />
+        </div>
+    )
+}
+
+export default Board;
