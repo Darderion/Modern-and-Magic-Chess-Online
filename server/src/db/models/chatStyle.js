7867b0ae45c5c154fd365e328fd7ccf43947e938
firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2022-07-23 23:54:21 +0300

    Add chatStyle
    fix bug in user model, add chatStyle

--- server/src/db/models/chatStyle.js
+++ server/src/db/models/chatStyle.js
@@ -3,33 +3,34 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class History extends Model {
+  class ChatStyle extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({User}) {
+      this.hasMany(User, {foreignKey: 'chatStyleId'});
     }
   }
-  History.init({
-    gameId: {
+  chatStyle.init({
+    id: {
       type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    fontColor: {
+      type: DataTypes.STRING,
       allowNull: false
     },
-    moveNum: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
+    backColor: {
+      type: DataTypes.STRING,
       allowNull: false
     }
   }, {
     sequelize,
-    tableName: 'history',
-    modelName: 'History',
+    tableName: 'chatStyle',
+    modelName: 'ChatStyle',
   });
-  return History;
+  return ChatStyle;
 };
