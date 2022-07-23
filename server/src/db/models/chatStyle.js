b5fe1001e3b112f01f23ac3d066cdd4bee11b484
Vadim <toadharvard@gmail.com>
2022-07-24 00:29:26 +0300

    Fix: uppercase to lowercase class name

--- server/src/db/models/chatStyle.js
+++ server/src/db/models/chatStyle.js
@@ -1,36 +1,37 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class ChatStyle extends Model {
+  class chatStyle extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User}) {
-      this.hasMany(User, {foreignKey: 'chatStyleId'});
+    static associate({ User }) {
+      this.hasMany(User, { foreignKey: 'chatStyleId' });
     }
   }
-  chatStyle.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+  chatStyle.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      fontColor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      backColor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    fontColor: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    backColor: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      sequelize,
+      tableName: 'chatStyle',
+      modelName: 'ChatStyle',
     }
-  }, {
-    sequelize,
-    tableName: 'chatStyle',
-    modelName: 'ChatStyle',
-  });
+  );
   return ChatStyle;
 };
