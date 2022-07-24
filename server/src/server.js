c640ef947785deac005d0947a48fff481dbaa652
max-fofanov <max.fofanov@gmail.com>
2022-07-24 16:23:19 +0300

    fixed simple history api

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const express = require('express');
 const config = require('./config');
-const { apiRouter, mainRouter, authRouter } = require('./routers');
+const { apiRouter, mainRouter, authRouter, historyRouter } = require('./routers');
 const setupMiddlewares = require('./middlewares');
 
 const app = express();
 
 setupMiddlewares(app);
 
+app.use('/history', historyRouter);
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 app.use('/', mainRouter);
+
 
 const server = http.createServer(app);
 
