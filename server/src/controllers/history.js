2a53a3697b737f91441de669e6d85cd37ccf614b
max-fofanov <max.fofanov@gmail.com>
2022-07-24 16:26:01 +0300

    prettier

--- server/src/controllers/history.js
+++ server/src/controllers/history.js
@@ -1,53 +1,47 @@
 const { Game } = require('../db/models');
 
-
 class HistoryController {
+  async getGameHistory(req, res) {
+    const id = req.params.id;
 
-    async getGameHistory(req, res) {
-        const id = req.params.id;
+    const game = await Game.findOne({
+      where: {
+        id,
+      },
+    });
 
-
-        const game = await Game.findOne( {
-            where : {
-                id
-            }
-        });
-
-        if (!game) {
-            res.json("No games matches id");
-        }
-        else {
-            res.json(game);
-        }
-
+    if (!game) {
+      res.json('No games matches id');
+    } else {
+      res.json(game);
     }
+  }
 
-    async setGameHistory(req, res) {
-        const id = req.params.id;
-        const {description} = req.body;
+  async setGameHistory(req, res) {
+    const id = req.params.id;
+    const { description } = req.body;
 
-        const game = await Game.findOne( {
-            where : {
-                id
-            }
-        });
+    const game = await Game.findOne({
+      where: {
+        id,
+      },
+    });
 
-        if (!game) {
-            res.json("No games matches id");
+    if (!game) {
+      res.json('No games matches id');
+    } else {
+      const updatedInfo = await Game.update(
+        {
+          description,
+        },
+        {
+          where: id,
         }
-        else {
-            const updatedInfo = await Game.update({
-                description
-            },
-            {
-                where :  id
-            });
+      );
 
-            res.json(updatedInfo);
-        }
+      res.json(updatedInfo);
     }
-
-
+  }
 }
 
 module.exports = new HistoryController();
