cac1aa5568bd054e8b3077540c7cc81b65cecae2
max-fofanov <max.fofanov@gmail.com>
2022-07-24 13:28:56 +0300

    added simple api for working with game description

--- server/src/routers.js
+++ server/src/routers.js
@@ -6,9 +6,11 @@ const { auth } = require('./controllers');
 const getAccessToken = require('./middlewares/getAccessToken');
 const verifyAccess = require('./middlewares/verifyAccess');
 const setRefreshToken = require('./middlewares/setRefreshToken');
+const historyController = require('./controllers/history');
 const apiRouter = new express.Router();
 const mainRouter = new express.Router();
 const authRouter = new express.Router();
+const historyRouter = new express.Router();
 
 /*
 apiRouter.get('/lobbies', verifyAccess, api.getLobbies);
@@ -22,6 +24,9 @@ authRouter.post('/login', auth.login, setRefreshToken, getAccessToken);
 authRouter.post('/refresh', auth.refresh, setRefreshToken, getAccessToken);
 authRouter.get('/logout', auth.logout);
 mainRouter.get('/ping', verifyAccess, ping);
+historyRouter.get('/get_history/:id', historyController.getGameHistory);
+historyRouter.post('/set_history/:id', historyController.setGameHistory);
+
 
 exports.mainRouter = mainRouter;
 exports.apiRouter = apiRouter;
