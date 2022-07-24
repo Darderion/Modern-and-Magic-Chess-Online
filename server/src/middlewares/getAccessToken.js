b2ebf90f6aa1d3b9438bc4f2694f88a275768464
Vadim <toadharvard@gmail.com>
2022-07-24 12:03:21 +0300

    Changed process.env to config.env

--- server/src/middlewares/getAccessToken.js
+++ server/src/middlewares/getAccessToken.js
@@ -1,16 +1,13 @@
 const { ApiError } = require('../validators/errors/ApiError');
 const jwt = require('jsonwebtoken');
+const { env } = require('../config');
 module.exports = async (req, res, next) => {
   const user = req.user;
   if (!user) new ApiError(400, 'User must be provided').sendResponse(res);
   try {
-    const accessToken = jwt.sign(
-      { id: req.user.id },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '1h',
-      }
-    );
+    const accessToken = jwt.sign({ id: req.user.id }, env.accessTokenSecret, {
+      expiresIn: '1h',
+    });
 
     res.json({
       accessToken,
