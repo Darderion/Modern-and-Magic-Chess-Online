12d65c05771e11f18afbd2a36ab302024941ef0e
yakovypg <yasha-pmp@mail.ru>
2022-07-25 07:38:47 +0300

    Changed errors structure, added api to index

--- server/src/validators/errors/ConnectionError.js
+++ server/src/validators/errors/ConnectionError.js
@@ -1,12 +1,8 @@
-class ConnectionError extends Error {
+const { ApiError } = require('./ApiError');
+
+class ConnectionError extends ApiError {
   constructor(status, message) {
-    super(message);
-
-    this.status = status;
-  }
-
-  sendResponse(res) {
-    return res.status(this.status).json({ message: this.message });
+    super(status, message);
   }
 }
 
