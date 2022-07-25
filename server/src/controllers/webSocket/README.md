04cccee0dbd419f3a1c17d9a98413d18fbf661a3
firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2022-07-25 13:00:16 +0300

    Change codeStyle with prettier, add README for webSockets on server

---
+++ server/src/controllers/webSocket/README.md
@@ -0,0 +1,12 @@
+# Использование webSoket-ов:
+- Открываем на клиенте веб-сокет по адресу ws://localhost:5000/home
+- Список возможных запросов к серверу и ответов от него (на клиенте это 'message' event у сокета): запрос -> ответ сервера
+    1) При открытии сокета: {"type":"connection","data":{"message":"Authorized as guest"},"code":200}
+    2) Подписаться на обновления открытых лобби: {"type":"allLobbies"} -> {"type":"allLobbies","data":[{"lobbyID":0,"lobbyName":"MyName","userName":"Mike"}],"code":200} 
+    3) Отписаться от открытых лобби: {"type":"unsubAllLobbies"} -> {"type":"unsubAllLobbies","code":200} 
+    4) Авторизоваться под user-ом с таким id в БД: {"type":"auth", "id":"1"} - !!! только для тестирования !!! webSocket умеет подцеплять ваш acessToken из кукисов, полученный при авторизации.
+    5) Открыть лобби: {"type":"openLobby", "data":{"lobbyName":"MyName"}} -> {"type":"openLobby","data":{"message":"Not authorized user"},"code":400}, {"type":"openLobby","data":{"message":"This user is in game"}, {"type":"openLobby","data":{"lobbyID":13,"lobbyName":"MyName"},"code":200}
+    6) Закрыть лобби: {"type":"closeLobby"} -> {"type":"closeLobby","data":"Lobby closed","code":200}, {"type":"closeLobby","data":{"message":"No open lobby"},"code":400}
+    7) Присоединиться к лобби с таким id: {"type":"connectToLobby","data":{"message":"Wrond lobbyID"},"code":400}, {"type":"createGame","data":{"gameID":1,"pgn":"[White \"Geo\"]\n[Black \"Mike\"]\n","isFirst":true,"styles":{}},"code":200} - стили тут появяться позже
+    8) Сделать ход: {"type":"myStep","data":{"move":"e2 e4"}} -> {"type":"myStep","data":{"message":"This user isn't in game or data wasn't sent to server"},"code":400}, {"type":"myStep","data":{"message":"Wrong step or not your step"},"code":400}, {"type":"myStep","data":{"pgn":"[White \"Geo\"]\n[Black \"Mike\"]\n\n1. e4 e5"},"code":200}
+    9) У другого игрока при ходе первого: {"type":"otherStep","data":{"pgn":"[White \"Geo\"]\n[Black \"Mike\"]\n\n1. e4 e5"},"code":200}
--- server/src/controllers/webSocket/gameMaster.js
+++ server/src/controllers/webSocket/gameMaster.js
@@ -2,61 +2,61 @@ const { Chess } = require('chess.js');
 const { Game, sequelize } = require('../../db/models');
 
 class GameMaster {
-    constructor(ws1, ws2, serverInfo, callback, reject = console.log) {
-        this.ws1 = ws1;
-        this.ws2 = ws2;
-        this.serverInfo = serverInfo;
-        this.chess = new Chess();
-        this.isFirstFirst = true;
-        //пока так, потом сделаем выбор первого хода
-        this.chess.header('White', ws1.user.nick);
-        this.chess.header('Black', ws2.user.nick);
-        this.currentStep = ws1;
-        try {
-            (async () => {
-                const newGame = await Game.create({
-                    firstUser: ws1.user.id,
-                    secondUser: ws2.user.id,
-                    startTime: sequelize.fn('NOW'),
-                    isFinished: 0,
-                    description: this.chess.pgn()
-                });
-                callback(newGame.id, this.chess.pgn());
-                this.game = newGame;
-            })();
-        } catch (e) {
-            reject(e.message);
+  constructor(ws1, ws2, serverInfo, callback, reject = console.log) {
+    this.ws1 = ws1;
+    this.ws2 = ws2;
+    this.serverInfo = serverInfo;
+    this.chess = new Chess();
+    this.isFirstFirst = true;
+    //пока так, потом сделаем выбор первого хода
+    this.chess.header('White', ws1.user.nick);
+    this.chess.header('Black', ws2.user.nick);
+    this.currentStep = ws1;
+    try {
+      (async () => {
+        const newGame = await Game.create({
+          firstUser: ws1.user.id,
+          secondUser: ws2.user.id,
+          startTime: sequelize.fn('NOW'),
+          isFinished: 0,
+          description: this.chess.pgn(),
+        });
+        callback(newGame.id, this.chess.pgn());
+        this.game = newGame;
+      })();
+    } catch (e) {
+      reject(e.message);
+    }
+  }
+  getOtherWS(ws) {
+    return this.ws1 === ws ? this.ws2 : this.ws1;
+  }
+  move(ws, move) {
+    if (ws === this.currentStep) {
+      const move2 = this.chess.move(move, { sloppy: true });
+      console.log(move2);
+      if (move2) {
+        if (this.chess.in_checkmate()) {
+          this.game.update({
+            description: this.chess.pgn(),
+            isFinished: 1,
+            winnerId: ws.user.id,
+            finishTime: sequelize.fn('NOW'),
+          });
         }
+        this.currentStep = ws === this.ws1 ? this.ws2 : this.ws1;
+        return this.chess.pgn();
+      }
     }
-    getOtherWS(ws) {
-        return this.ws1 === ws ? this.ws2 : this.ws1;
+    return undefined;
+  }
+  save() {
+    if (!this.game.isFinished) {
+      this.game.update({
+        description: this.chess.pgn(),
+      });
     }
-    move(ws, move) {
-        if(ws === this.currentStep) {
-            const move2 = this.chess.move(move, { sloppy: true });
-            console.log(move2);
-            if(move2) {
-                if(this.chess.in_checkmate()) {
-                    this.game.update({
-                        description: this.chess.pgn(),
-                        isFinished: 1,
-                        winnerId: ws.user.id,
-                        finishTime: sequelize.fn('NOW')
-                    });
-                }
-                this.currentStep = ws === this.ws1 ? this.ws2 : this.ws1;
-                return this.chess.pgn();
-            }
-        }
-        return undefined;
-    }
-    save() {
-        if(!this.game.isFinished) {
-            this.game.update({
-                description: this.chess.pgn()
-            });
-        }
-    }
+  }
 }
 
 module.exports = GameMaster;
