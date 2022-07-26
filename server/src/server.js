6a64dae3dafdc84e8efe8c58d12598291e692647
Pechenux <belokoniy@gmail.com>
2022-07-26 17:58:01 +0300

    Fixed ws conflicts

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,21 @@
 const http = require('http');
 const express = require('express');
-const swaggerUI = require('swagger-ui-express');
 const config = require('./config');
 const { apiRouter, mainRouter, authRouter } = require('./routers');
 const setupMiddlewares = require('./middlewares');
-const swaggerSpec = require('./docs/index');
+const path = require("path");
+const mainWS = require('./controllers/webSocket/mainWS');
 
 const app = express();
 
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-
 setupMiddlewares(app);
-
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 app.use('/', mainRouter);
 
 const server = http.createServer(app);
-
+mainWS(server);
 const { port } = config.server;
 
 server.listen(port, () => {
