eaeeb2c1bbf30f039de9881a5843f00350208934
firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2022-07-26 20:01:50 +0300

    Create lobby route

--- webapp/src/components/Nav/Nav.jsx
+++ webapp/src/components/Nav/Nav.jsx
@@ -14,11 +14,17 @@ export default function Nav() {
 						Home
 					</NavComponent.Link>
 <<<<<<< HEAD
+<<<<<<< HEAD
 					<NavComponent.Link as={Link} to="/Lobby">
 						Lobby
 					</NavComponent.Link>
 =======
 >>>>>>> 6750ffd (Add inventory, add styles on server, add static support on server, add webSocketSuppert on server.)
+=======
+					<NavComponent.Link as={Link} to="/Lobby">
+						Lobby
+					</NavComponent.Link>
+>>>>>>> 675e7ed (Create lobby route)
 					<NavComponent.Link as={Link} to="/Inventory">
 						Inventory
 					</NavComponent.Link>
