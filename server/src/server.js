6750ffd0aa3f64d1bf9f72fc3c34d7e967583499
firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2022-07-26 14:03:19 +0300

    Add inventory, add styles on server, add static support on server, add webSocketSuppert on server.

--- server/src/server.js
+++ server/src/server.js
@@ -3,17 +3,19 @@ const express = require('express');
 const config = require('./config');
 const { apiRouter, mainRouter, authRouter } = require('./routers');
 const setupMiddlewares = require('./middlewares');
+const path = require("path");
+const mainWS = require('./controllers/webSocket/mainWS');
 
 const app = express();
 
 setupMiddlewares(app);
-
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 app.use('/', mainRouter);
 
 const server = http.createServer(app);
-
+mainWS(server);
 const { port } = config.server;
 
 server.listen(port, () => {
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,6 +3,7 @@ import { Nav } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './routes/About/About'
 import Main from './routes/Main/Main'
+import Inventory from './routes/Inventory/Inventory';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Main></Main>}></Route>
 					<Route path="/about" element={<About></About>}></Route>
+					<Route path="/inventory" element={<Inventory></Inventory>}></Route>
 				</Routes>
 			</div>
 		</BrowserRouter>
