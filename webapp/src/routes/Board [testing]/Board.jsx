748b6b5d17b6e0aa052e62cbea92ff4dcc83882a
Darderion <Eobard1SA@gmail.com>
2022-07-27 06:42:00 +0300

    npx eslint files

--- webapp/src/routes/Board [testing]/Board.jsx
+++ webapp/src/routes/Board [testing]/Board.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
 import * as Chess from 'chess.js';
-import BoardComponent from './BoardComponent'
-import './style.css'
-import ChessContext from './ChessContext'
+import BoardComponent from './BoardComponent';
+import './style.css';
+import ChessContext from './ChessContext';
 
 const Board = () => {
-
-	const [ chess, setChess ] = useState(new Chess())
+	const [chess, setChess] = useState(new Chess());
 
 	return (
-		<ChessContext.Provider value={{chess, setChess}}>
-		<div className="wrapper">
-			<div className='board-container'><BoardComponent
-				view={'white'}
-			/>White</div>
-			<div className='board-container'><BoardComponent
-				view={'black'}
-			/>Black</div>
-			<div className='board-container'><BoardComponent
-				view={'observer'}
-			/>Observer</div>
-		</div>
+		<ChessContext.Provider value={{ chess, setChess }}>
+			<div className="wrapper">
+				<div className="board-container">
+					<BoardComponent view={'white'} />
+					White
+				</div>
+				<div className="board-container">
+					<BoardComponent view={'black'} />
+					Black
+				</div>
+				<div className="board-container">
+					<BoardComponent view={'observer'} />
+					Observer
+				</div>
+			</div>
 		</ChessContext.Provider>
 	);
 };
--- webapp/src/routes/Board [testing]/BoardContext.jsx
+++ webapp/src/routes/Board [testing]/BoardContext.jsx
@@ -1,5 +1,5 @@
-import { createContext } from "react"
+import { createContext } from 'react';
 
-const BoardContext = createContext()
+const BoardContext = createContext();
 
 export default BoardContext;
