4f2ad75bf1e2c19a754787bdafc899dac9b64fea
firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2022-07-27 16:48:22 +0300

    Add create/delete lobby form in lobbies

--- webapp/src/routes/Lobby/Lobby.jsx
+++ webapp/src/routes/Lobby/Lobby.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import './Lobby.css';
+import TextareaAutosize from 'react-textarea-autosize';
+import { useState } from 'react';
 
 export default function Main({ lobbies }) {
+  const [ lobby, setLobby ] = useState({id: 10, desc: 'TestTestTestTestTestTest dasgh dgsha gd hsg ahfsdg hfjdsa ghjf sgadhjfg shadgf hsadgh fsdagh fsgadh jfasgdj ghj'});
   const playGame = (lobbyId) => {
     console.log('Join lobby = ' + lobbyId);
   }
+
+  const createLobby = () => {
+    const textarea =  document.querySelector('.create__lobby__description');
+    //send desc on server;
+    setLobby(() => {
+      return {id: 1, desc: textarea.value}
+    });
+  }
+  const closeLobby = () => {
+    //send Toserver lobbyID
+    setLobby(() => {
+      return {id: undefined, desc: ''}
+    });
+  }
+
 	return (
 		<div className="lobby__container">
+      { !(lobby.id + 1) ? 
+      <div className="create__lobby">
+        <TextareaAutosize className="create__lobby__description" 
+          placeholder="Description"/>
+        <button className="create__lobby__btn" onClick={createLobby}>Create lobby</button>
+      </div> : 
+      <div className="wait__player">
+        <div className="player__lobby__info">
+          <div className="wait__title">Wait other player connection to your lobby.</div>
+          <div className="lobby__desc" id="lobbyDesc">Description: {lobby.desc}</div>
+        </div>
+        <button className="close__lobby" onClick={closeLobby}>X</button>
+      </div>
+      }
       <div className="open__lobbies">
         {lobbies.map(({id, userName, lobbyName}) => {
           return (<div className="lobby__info" key={id}>
@@ -15,8 +47,8 @@ export default function Main({ lobbies }) {
               <div className="lobbyname">{lobbyName}</div>
             </div>
             <button className="lobby__info__btn" onClick={() => playGame(id)}>
-                Играть
-             </button>
+              Play
+           </button>
           </div>
           );
         })}
