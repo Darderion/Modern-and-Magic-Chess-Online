1532c19b8c4bdb06f5779154e50f9b2d983e4d6e
astatochek <71458498+astatochek@users.noreply.github.com>
2022-07-28 21:38:11 +0300

    some logic added

--- webapp/src/components/Game/GameComponent.jsx
+++ webapp/src/components/Game/GameComponent.jsx
@@ -1,37 +1,64 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useMemo, useState, useEffect } from 'react';
 import { ConnectorContext } from '../../Connector';
 import BoardComponent from '../Board/BoardComponent';
-import * as Chess from 'chess.js'
-import './styles.css'
+import * as Chess from 'chess.js';
+import axios from 'axios';
+import './styles.css';
 
-const GameComponent = ({skins}) => {
-    const { lobbyData, boardData, sendMessage } = useContext(ConnectorContext);
-    const chess = new Chess()
-    return useMemo(
-        () => {
-            return (
-                <div className='wrapper'>
-                    <div className='window'>
-                        <div className='left-container'>
-                        <div className='board-container'>
-                            <BoardComponent chess={chess} view='white' skins={skins} />
-                        </div>
-                        </div>
-                        <div className='right-container'>
-                            <div className='info'>
-                                <div className='block'>Current Turn: White</div>
-                                <div className='block'>Surrender</div>
-                            </div>
-                            <div className='text-block'>
-                                <p>Player1: Hello there.</p>
-                                <p>Player2: General Kenobi!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-        }, [boardData, lobbyData]
-    )
-}
+const GameComponent = ({ pgn, skins }) => {
+
+	const { lobbyData, boardData, sendMessage } = useContext(ConnectorContext);
+
+	const {chess, setChess} = useState(new Chess());
+
+    useEffect((prev) => {
+        const next = new Chess();
+        next.load_pgn(pgn.join('\n'));
+        return next;
+    }, [])
+
+    useEffect(() => {
+        const newPGN = null; // TODO: получение pgn из boardData
+        const next = new Chess();
+        next.load_pgn(newPGN.join('\n'));
+        return next;
+    }, [boardData])
+
+	const handleSurrenderClick = () => {
+		// ???
+	};
+
+    const currentTurn = String(chess.turn() === 'b') ? 'Black' : 'White';
+
+    const view = null; // TODO: добавить получение смотрящей стороны
+
+	return useMemo(() => {
+		return (
+			<div className="wrapper">
+				<div className="window">
+					<div className="left-container">
+						<div className="board-container">
+							<BoardComponent chess={chess} view={view} skins={skins} />
+						</div>
+					</div>
+					<div className="right-container">
+						<div className="info">
+							<div className="block">Current Turn: {currentTurn}</div>
+							<button class="button" onClick={handleSurrenderClick}>
+								<span>Surrender </span>
+							</button>
+						</div>
+						<div className="text-block">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+							natus unde nesciunt itaque ea aliquam dolorum sint officia sunt
+							repellendus expedita esse, minima neque amet molestiae enim et
+							suscipit iure!
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}, [boardData, lobbyData]);
+};
 
 export default GameComponent;
