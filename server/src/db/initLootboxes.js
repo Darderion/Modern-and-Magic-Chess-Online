57d3a4016d3576af29efcdd500b2807ae2ea4018
yakovypg <yasha-pmp@mail.ru>
2022-07-28 19:47:36 +0300

    add initializing lootboxes

---
+++ server/src/db/initLootboxes.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('../util/fs');
+
+const LootboxCreator = require('../controllers/sharedFunctions/LootboxCreator');
+const StyleDispatcher = require('../controllers/sharedFunctions/StyleDispatcher');
+
+const main = async () => {
+  const packsPath = path.resolve(__dirname, '../static/skins');
+  const allPacks = fs.getAllFilesInFolder(packsPath).dirs;
+  const packs = allPacks.filter((t) => t !== 'default');
+
+  for (let packName of packs) {
+    const operationInfo = await StyleDispatcher.getStyles({ packName });
+
+    if (!operationInfo.success){
+      continue;
+    }
+
+    const price = 100;
+    const stylesIds = operationInfo.styles.map((t) => t.id);
+
+    LootboxCreator.create(price, stylesIds);
+  }
+};
+
+main();
