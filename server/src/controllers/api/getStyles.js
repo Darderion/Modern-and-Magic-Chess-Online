0054a55f9c23f4da11dd90b0bf2b86adb3fb6c6e
Vadim <toadharvard@gmail.com>
2022-07-28 18:25:48 +0300

    Update getStyles.js

--- server/src/controllers/api/getStyles.js
+++ server/src/controllers/api/getStyles.js
@@ -1,15 +1,13 @@
-const { Style } = require('../../db/models');
-const { ApiError } = require('../../validators/errors/ApiError');
 const { getStyles } = require('../sharedFunctions/getStyles');
 
-module.exports = async (req, res, next) => {
+module.exports = async (req, res) => {
   const where = {};
   if (req?.body?.id) where.id = req.body.id;
   if (req?.body?.pieceColor) where.pieceColor = req.body.pieceColor;
   if (req?.body?.typeOfPiece) where.typeOfPiece = req.body.typeOfPiece;
   if (req?.body?.packName) where.packName = req.body.packName;
   try {
-    await getStyles(where);
+    return res.json({ styles: await getStyles(where) });
   } catch (e) {
     return e.sendResponse(res);
   }
