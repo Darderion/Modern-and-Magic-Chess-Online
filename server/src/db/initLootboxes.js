76bdb0a7a43da239422fc369578132af737658db
yakovypg <yasha-pmp@mail.ru>
2022-07-29 01:40:47 +0300

    Clean initLootboxes.js code

--- server/src/db/initLootboxes.js
+++ server/src/db/initLootboxes.js
@@ -7,15 +7,15 @@ const StyleDispatcher = require('../controllers/sharedFunctions/StyleDispatcher'
 const main = async () => {
   const packsPath = path.resolve(__dirname, '../static/skins');
   const allPacks = fs.getAllFilesInFolder(packsPath).dirs;
-  
+
   const packs = allPacks
-    .map(t => t.replace(/^.*[\\\/]/, ''))
+    .map((t) => t.replace(/^.*[\\\/]/, ''))
     .filter((t) => t !== 'default');
 
   for (let packName of packs) {
     const operationInfo = await StyleDispatcher.getStyles({ packName });
 
-    if (!operationInfo.success){
+    if (!operationInfo.success) {
       continue;
     }
 
