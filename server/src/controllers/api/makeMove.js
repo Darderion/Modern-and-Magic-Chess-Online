1caa33f3fd5055ef18dfe6ab6a4c656356948598
yakovypg <yasha-pmp@mail.ru>
2022-07-28 22:58:27 +0300

    Add adding profits after the end of the game

--- server/src/controllers/api/makeMove.js
+++ server/src/controllers/api/makeMove.js
@@ -7,6 +7,7 @@ const {
 const { PgnLoadingChessError } = require('../../validators/errors/ChessError');
 
 const GameExecutor = require('../sharedFunctions/GameExecutor');
+const UserDispatcher = require('../sharedFunctions/UserDispatcher');
 
 // TEST
 // curl -X POST -H "Content-Type: application/json" -d '{"lobbyId": "2", "from": "a2", "to": "a3"}' http://localhost:5000/api/makeMove
@@ -15,18 +16,21 @@ module.exports = async (req, res) => {
   const { lobbyId, from, to } = req.body;
 
   let gameModel;
-  let pgn;
+  let whitePiecesUserId, blackPiecesUserId, pgn;
 
   const game = new Chess();
 
   try {
     const gameModels = await Game.findAll({
-      attributes: ['description'],
+      attributes: ['whitePiecesUserId', 'blackPiecesUserId', 'description'],
       where: { id: lobbyId },
     });
 
     gameModel = gameModels[0];
+
     pgn = gameModel.description;
+    whitePiecesUserId = gameModel.whitePiecesUserId;
+    blackPiecesUserId = gameModel.blackPiecesUserId;
 
     if (!game.load_pgn(pgn)) {
       const pgnError = new PgnLoadingChessError();
@@ -46,6 +50,35 @@ module.exports = async (req, res) => {
   if (!success) {
     return error.sendResponse(res);
   }
+
+  if (fieldsToUpdate.result) {   
+        let whitePlayerProfit, blackPlayerProfit;
+    
+        if (fieldsToUpdate.result === 'white') {
+          whitePlayerProfit = 100;
+          blackPlayerProfit = 25;
+        }
+        else if (fieldsToUpdate.result === 'black') {
+          whitePlayerProfit = 25;
+          blackPlayerProfit = 100;
+        }
+        else {
+          whitePlayerProfit = 50;
+          blackPlayerProfit = 50;
+        }
+    
+        const opInfo1 = await UserDispatcher.increaseMoney(whitePiecesUserId, whitePlayerProfit);
+    
+        if (!opInfo1.success) {
+          return opInfo1.error.sendResponse(res);
+        }
+    
+        const opInfo2 = await UserDispatcher.increaseMoney(blackPiecesUserId, blackPlayerProfit);
+    
+        if (!opInfo2.success) {
+          return opInfo2.error.sendResponse(res);
+        }
+      }
 
   try {
     await Game.update(fieldsToUpdate, {
