8cfe51b64265ea1fb0106dee63cc895767d1b2f7
yakovypg <yasha-pmp@mail.ru>
2022-07-28 19:46:22 +0300

    Add StyleDispatcher

--- server/src/controllers/api/getStyles.js
+++ server/src/controllers/api/getStyles.js
@@ -1,14 +1,17 @@
-const { getStyles } = require('../sharedFunctions/getStyles');
+const { ApiError } = require('../../validators/errors/ApiError');
+const StyleDispatcher = require('../sharedFunctions/StyleDispatcher');
 
-module.exports = async (req, res) => {
-  const where = {};
-  if (req?.body?.id) where.id = req.body.id;
-  if (req?.body?.pieceColor) where.pieceColor = req.body.pieceColor;
-  if (req?.body?.typeOfPiece) where.typeOfPiece = req.body.typeOfPiece;
-  if (req?.body?.packName) where.packName = req.body.packName;
-  try {
-    return res.json({ styles: await getStyles(where) });
-  } catch (e) {
-    return e.sendResponse(res);
+module.exports = async (req, res, next) => {
+  if (!req || !req.body) {
+    return new ApiError(
+      500,
+      'Incorrect request: req or req.body is not valid'
+    ).sendResponse(res);
   }
+
+  const operationInfo = await StyleDispatcher.getStyles(req.body);
+
+  return operationInfo.success
+    ? res.json(operationInfo.styles)
+    : operationInfo.error.sendResponse(res);
 };
